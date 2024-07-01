6fb771a6c67e5fbcd32ab3c909d097bd1909dbb6
Alanssrv <alanssrv@gmail.com>
2024-07-01 20:13:16 -0300

    feat: modelos da base de dados e migrations

---
+++ GeekShopping.ProductAPI/Model/Context/SQLContext.cs
@@ -0,0 +1,21 @@
+﻿using GeekShopping.ProductAPI.Model.Base;
+using Microsoft.EntityFrameworkCore;
+
+namespace GeekShopping.ProductAPI.Model.Context
+{
+    public class SQLContext : DbContext
+    {
+        protected readonly IConfiguration Configuration;
+
+        public SQLContext(IConfiguration configuration) {
+            Configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(Configuration.GetConnectionString("DatabaseConnection"));
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
