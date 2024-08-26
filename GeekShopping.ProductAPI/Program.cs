a4238bf5bfd3c6a49348a67449b75e1175f3debe
Alanssrv <alanssrv@gmail.com>
2024-08-26 20:32:30 -0300

    feat: criacao da controller, vo e repository de Products e nova migration com insercao de registros

--- GeekShopping.ProductAPI/Program.cs
+++ GeekShopping.ProductAPI/Program.cs
@@ -1,6 +1,21 @@
+using AutoMapper;
+using GeekShopping.ProductAPI.Config;
+using GeekShopping.ProductAPI.Model.Context;
+using GeekShopping.ProductAPI.Repository;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
+var connection = builder.Configuration["ConnectionStrings:DatabaseConnection"];
+
+builder.Services.AddDbContext<SQLContext>(options => options.UseSqlServer(connection));
+
 // Add services to the container.
+IMapper mapper = MappingConfig.RegisterMaps().CreateMapper();
+builder.Services.AddSingleton(mapper);
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
