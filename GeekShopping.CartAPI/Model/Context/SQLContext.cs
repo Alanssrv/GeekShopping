109ecda4a5fbf497bc6d164944c7f7eff9f0906a
Alanssrv <alanssrv@gmail.com>
2024-10-29 20:35:39 -0300

    feat: estrutura base da CartAPI, entidades e migrations

---
+++ GeekShopping.CartAPI/Model/Context/SQLContext.cs
@@ -0,0 +1,23 @@
+﻿using GeekShopping.CartAPI.Model.Base;
+using Microsoft.EntityFrameworkCore;
+
+namespace GeekShopping.CartAPI.Model.Context
+{
+    public class SQLContext : DbContext
+    {
+        protected readonly IConfiguration Configuration;
+
+        public SQLContext(IConfiguration configuration) {
+            Configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(Configuration.GetConnectionString("DatabaseConnection"));
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<CartDetail> CartDetails { get; set; }
+        public DbSet<CartHeader> CartHeaders { get; set; }
+    }
+}
