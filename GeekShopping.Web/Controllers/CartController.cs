1dd180092a775201c876dca31c97589b7947c60c
Alanssrv <alanssrv@gmail.com>
2024-11-11 21:32:09 -0300

    feat(web): implementacao dos metodos apply e remove coupon

--- GeekShopping.Web/Controllers/CartController.cs
+++ GeekShopping.Web/Controllers/CartController.cs
@@ -53,5 +53,35 @@ namespace GeekShopping.Web.Controllers
 
             return response;
         }
+
+        [HttpPost]
+        [ActionName("ApplyCoupon")]
+        public async Task<IActionResult> ApplyCoupon(CartViewModel model)
+        {
+            var accessToken = await HttpContext.GetTokenAsync("access_token");
+            var userId = User.Claims.Where(user => user.Type == "sub")?.FirstOrDefault()?.Value;
+
+            var response = await _cartService.ApplyCoupon(model, accessToken);
+
+            if (response)
+                return RedirectToAction(nameof(CartIndex));
+
+            return View();
+        }
+
+        [HttpPost]
+        [ActionName("RemoveCoupon")]
+        public async Task<IActionResult> RemoveCoupon()
+        {
+            var accessToken = await HttpContext.GetTokenAsync("access_token");
+            var userId = User.Claims.Where(user => user.Type == "sub")?.FirstOrDefault()?.Value;
+
+            var response = await _cartService.RemoveCoupon(userId, accessToken);
+
+            if (response)
+                return RedirectToAction(nameof(CartIndex));
+
+            return View();
+        }
     }
 }
